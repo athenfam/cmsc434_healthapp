68993497e3197e1b2cf2efc69467276b5bcc2be1
athenfam <athenfam@gmail.com>
2022-03-18 17:57:18 -0400

    Add nutrition button

--- app/src/main/java/com/example/healthapp/NutritionFragment.java
+++ app/src/main/java/com/example/healthapp/NutritionFragment.java
@@ -1,18 +1,35 @@
 package com.example.healthapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class NutritionFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_nutrition, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = (Button) getView().findViewById(R.id.addData);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(),AddNutritionPopUp.class));
+            }
+        });
+
     }
 }
