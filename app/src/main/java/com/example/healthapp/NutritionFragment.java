62cbf126ba40c1362e63562f8a61611e08bc8ca6
athenfam <athenfam@gmail.com>
2022-03-18 19:06:27 -0400

    Added Input box w/o updated total calorie count

--- app/src/main/java/com/example/healthapp/NutritionFragment.java
+++ app/src/main/java/com/example/healthapp/NutritionFragment.java
@@ -6,12 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class NutritionFragment extends Fragment {
+
+    int targetCalories = 200;
+    int consumedCalories = 0;
 
     @Nullable
     @Override
@@ -23,11 +27,21 @@ public class NutritionFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        TextView totalCaloriesConsumedTextView = (TextView) getView().findViewById(R.id.totalCaloriesConsumed);
+        totalCaloriesConsumedTextView.setText("Total Calories Consumed: " + consumedCalories);
+
         Button button = (Button) getView().findViewById(R.id.addData);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(),AddNutritionPopUp.class));
+                if(getArguments() != null) {
+                    consumedCalories += Integer.valueOf(getArguments().getString("caloriesConsumed"));
+                    totalCaloriesConsumedTextView.setText("Total Calories Consumed: " + consumedCalories);
+                    System.out.println("recieved argument: " + consumedCalories);
+                } else {
+                    System.out.println("no arg");
+                }
             }
         });
 
